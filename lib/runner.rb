5205f65d52400bc369ce909feff6ee72c693f909
github username <github email address>
2018-09-26 16:09:17 -0400

    Done.

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,11 +1,3 @@
 require_relative "blackjack.rb"
 
-def runner
-  welcome
-  total = initial_round
-  total = hit? (total)
-  display_card_total(total)
-  until total > 21
-  end
-end_game (total)
-end
+runner
